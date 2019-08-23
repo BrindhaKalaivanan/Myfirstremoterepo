a797b2985775e67ab596c3c1a89577317fff3a23
brindha <brindhakalaivanan@gmail.com>
2019-08-23 22:18:42 +0530

    recommit

--- src/main.py
+++ src/main.py
@@ -1,8 +1,5 @@
 --Learning DevOps,AWS,Python,Linux
 ## User login 1
 ## User Enter user name
-<<<<<<< HEAD
-## User enter password
-=======
 ## Wd Release 1 changes are on track
->>>>>>> 4dbaf73ea07fd2159e25f62f6cde14a74409510a
+
